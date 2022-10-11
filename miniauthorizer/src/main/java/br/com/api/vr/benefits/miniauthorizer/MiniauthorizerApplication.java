ed39e935bf27c2af44fb40bf3ea514ffdc6c11eb
luiz.fonseca <luiz.fonseca@e-unicred.com.br>
2022-10-11 11:09:34 -0300

    Gerado o projeto através do spring initializr

---
+++ miniauthorizer/src/main/java/br/com/api/vr/benefits/miniauthorizer/MiniauthorizerApplication.java
@@ -0,0 +1,13 @@
+package br.com.api.vr.benefits.miniauthorizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniauthorizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniauthorizerApplication.class, args);
+	}
+
+}
---
+++ miniauthorizer/src/test/java/br/com/api/vr/benefits/miniauthorizer/MiniauthorizerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.api.vr.benefits.miniauthorizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniauthorizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
